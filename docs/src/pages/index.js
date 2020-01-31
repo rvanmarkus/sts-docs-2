8cfa17db45ae02c476d6d9fbf9568e26a020a52c
Robbert van Markus <rvanmarkus@stackstate.com>
2020-01-31 12:05:54 +0100

    start working on homepage

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import styled from '@emotion/styled';
 import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
@@ -8,14 +9,42 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      {/* <Image /> */}
-    </div>
+    <h3>Start with these resources ...</h3>
+    <DocsCards>
+      <DocsCard>
+        <DocsCardTitle>
+          Getting started
+        </DocsCardTitle>
+        <DocsCardSubTitle>
+          Get going in just small steps
+        </DocsCardSubTitle>
+        <DocsCardLink>Download</DocsCardLink>
+        <DocsCardLink>Configuration</DocsCardLink>
+      </DocsCard>
+    </DocsCards>
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+
+const DocsCards = styled.div({
+  display: "grid"
+});
+
+const DocsCard = styled.div({
+
+});
+
+const DocsCardTitle = styled.h3({
+
+});
+
+const DocsCardLink = styled.h3({
+
+});
+
+const DocsCardSubTitle = styled.h4({
+
+});
 
 export default IndexPage
